c0c6a8cf71e479d943d47d21a563bff758234765
antklim <antklim@gmail.com>
2018-03-28 18:15:37 +1100

    go-kit microservice WIP

--- go-kit-greeter/main.go
+++ go-kit-greeter/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"context"
 	"errors"
+	proto "github.com/antklim/go-microservices/go-kit-greeter/proto"
+	"github.com/go-kit/kit/endpoint"
 	"strings"
 )
 
 type Greeter interface {
-	Hello(context.Context, string) (string, error)
+	Hello(ctx context.Context, in *proto.HelloRequest) (string, error)
 }
 
 type greeterService struct{}
 
-func (greeterService) Hello(_ context.Context, s string) (string, error) {
-	return "GO-KIT Hello " + s
+func (g *greeterService) Hello(ctx context.Context, in *proto.HelloRequest) (string, error) {
+	greeting := "GO-KIT Hello " + in.Name
+	return greeting, nil
+}
+
+func makeHelloEndpoint(svc Greeter) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(proto.HelloRequest)
+		greeting, err := svc.Hello(ctx, req)
+		if err != nil {
+			return proto.HelloResponse{}, err
+		}
+		return proto.HelloResponse{Greeting: greeting}, nil
+	}
+}
+
+func main() {
+	svc := greeterService{}
 }
