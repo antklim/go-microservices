7b59c10c442346b6ef85a7d4588db3225769987e
antklim <antklim@gmail.com>
2018-03-31 09:06:33 +1100

    go-kit-greeter: transport WIP

--- go-kit-greeter/transport.go
+++ go-kit-greeter/transport.go
@@ -1,8 +1,12 @@
 package gokitgreeter
 
 import (
+	"encoding/json"
 	"errors"
+	"github.com/go-kit/kit/log"
+	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 var (
@@ -10,6 +14,31 @@ var (
 	// It always indicates programmer error.
 	ErrBadRouting = errors.New("inconsistent mapping between route and handler (programmer error)")
 )
+
+func MakeHTTPHandler(s Service, logger log.Logger) http.Handler {
+	r := mux.NewRouter()
+	e := MakeServiceEndpoints(s)
+	options := []httptransport.ServerOption{
+		httptransport.ServerErrorLogger(logger),
+		httptransport.ServerErrorEncoder(encodeError),
+	}
+
+	// GET /health      retrieves service heath information
+	// GET /hello?name  retrieves greeting
+
+	r.Methods("GET").Path("/health").Handler(httptransport.NewServer(
+		e.GetHealthEndpoint,
+		decodeGetHealthRequest,
+		encodeResponse,
+		options...,
+	))
+	r.Methods("GET").Path("/hello").Handler(httptransport.NewServer(
+		e.GetGreetingEndpoint,
+		decodeGetGreeterRequest,
+		encodeResponse,
+		options...,
+	))
+}
 
 func decodeGetHealthRequest(_ context.Context, _ *http.Request) (interface{}, error) {
 	return getHealthRequest{}, nil
@@ -23,4 +52,27 @@ func decodeGetGreeterRequest(_ context.Context, r *http.Request) (interface{}, e
 	}
 
 	return getGreetingRequest{Name: name}, nil
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	if e, ok := response.(errorer); ok && e.error() != nil {
+		// Not a Go kit transport error, but a business-logic error.
+		// Provide those as HTTP errors.
+		encodeError(ctx, e.error(), w)
+		return nil
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	return json.NewEncoder(w).Encode(response)
+}
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	if err == nil {
+		panic("encodeError with nil error")
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(codeFrom(err))
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+	})
 }
