fb78efaefc2b0f2ddb2cc0782e9730c0575d832e
antklim <antklim@gmail.com>
2018-04-08 17:21:11 +1000

    go-kit-greeter: main updated

--- go-kit-greeter/main.go
+++ go-kit-greeter/main.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-	"context"
-	// "errors"
-	proto "github.com/antklim/go-microservices/go-kit-greeter/proto"
-	"github.com/go-kit/kit/endpoint"
-	// "strings"
+	"flag"
+	"github.com/go-kit/kit/log"
+	"os"
 )
 
-type Greeter interface {
-	Hello(ctx context.Context, in *proto.HelloRequest) (string, error)
-}
-
-type greeterService struct{}
-
-func (g *greeterService) Hello(ctx context.Context, in *proto.HelloRequest) (string, error) {
-	greeting := "GO-KIT Hello " + in.Name
-	return greeting, nil
-}
-
-func makeHelloEndpoint(svc Greeter) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(proto.HelloRequest)
-		greeting, err := svc.Hello(ctx, &req)
-		if err != nil {
-			return proto.HelloResponse{}, err
-		}
-		return proto.HelloResponse{Greeting: greeting}, nil
-	}
-}
-
 func main() {
-	svc := greeterService{}
-	helloEndpoint := makeHelloEndpoint(&svc)
-	helloEndpoint()
+	var (
+		httpAddr = flag.String("http.addr", ":8080", "HTTP Listen Address")
+	)
+	flag.Parse()
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	// var s Service {
+
+	// }
+
+	// var h http.Handler
+	// {
+
+	// }
+
+	errs := make(chan error)
+
+	logger.Log("exit", <-errs)
 }
