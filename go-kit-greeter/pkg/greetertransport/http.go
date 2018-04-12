23e1ce202fecf9a2999c120562ed5e8598c586a8
antklim <antklim@gmail.com>
2018-04-13 08:15:38 +1000

    go-kit-greeter: fixing transport

--- go-kit-greeter/pkg/greetertransport/http.go
+++ go-kit-greeter/pkg/greetertransport/http.go
@@ -3,12 +3,18 @@ package greetertransport
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/antklim/go-microservices/go-kit-greeter/pkg/greeterendpoint"
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
+)
+
+var (
+	// ErrBadRouting is returned when an expected path variable is missing.
+	ErrBadRouting = errors.New("inconsistent mapping between route and handler")
 )
 
 // NewHTTPHandler returns an HTTP handler that makes a set of endpoints
@@ -39,15 +45,18 @@ func NewHTTPHandler(endpoints greeterendpoint.Endpoints, logger log.Logger) http
 }
 
 func decodeHTTPGetHealthRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var req greeterendpoint.GetHealthRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	return req, err
+	req := greeterendpoint.GetHealthRequest{}
+	return req, nil
 }
 
 func decodeHTTPGetGreeterRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var req greeterendpoint.GetGreetingRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	return req, err
+	vars := mux.Vars(r)
+	name, ok := vars["name"]
+	if !ok {
+		return nil, ErrBadRouting
+	}
+	req := greeterendpoint.GetGreetingRequest{Name: name}
+	return req, nil
 }
 
 func encodeError(_ context.Context, err error, w http.ResponseWriter) {
