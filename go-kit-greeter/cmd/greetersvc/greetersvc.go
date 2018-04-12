03b3846ab6cc4d008d09fed01abdca006d656d82
antklim <antklim@gmail.com>
2018-04-12 16:08:56 +1000

    go-kit-greeter: service entry point

---
+++ go-kit-greeter/cmd/greetersvc/greetersvc.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"text/tabwriter"
+
+	"github.com/antklim/go-microservices/go-kit-greeter/pkg/greeterendpoint"
+	"github.com/antklim/go-microservices/go-kit-greeter/pkg/greeterservice"
+	"github.com/antklim/go-microservices/go-kit-greeter/pkg/greetertransport"
+
+	"github.com/go-kit/kit/log"
+	"github.com/oklog/oklog/pkg/group"
+)
+
+func main() {
+	fs := flag.NewFlagSet("greetersvc", flag.ExitOnError)
+	var (
+		debugAddr = fs.String("debug.addr", ":8080", "Debug and metrics listen address")
+		httpAddr  = fs.String("http.addr", ":8081", "HTTP Listen Address")
+	)
+	fs.Usage = usageFor(fs, os.Args[0]+" [flags]")
+	flag.Parse()
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	var (
+		service     = greeterservice.New(logger)
+		endpoints   = greeterendpoint.MakeEndpoints(service, logger)
+		httpHandler = greetertransport.NewHTTPHandler(endpoints, logger)
+	)
+
+	var g group.Group
+	{
+		// The debug listener mounts the http.DefaultServeMux, and serves up
+		// stuff like the Go debug and profiling routes, and so on.
+		debugListener, err := net.Listen("tcp", *debugAddr)
+		if err != nil {
+			logger.Log("transport", "debug/HTTP", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "debug/HTTP", "addr", *debugAddr)
+			return http.Serve(debugListener, http.DefaultServeMux)
+		}, func(error) {
+			debugListener.Close()
+		})
+	}
+	{
+		// The HTTP listener mounts the Go kit HTTP handler we created.
+		httpListener, err := net.Listen("tcp", *httpAddr)
+		if err != nil {
+			logger.Log("transport", "HTTP", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "HTTP", "addr", *httpAddr)
+			return http.Serve(httpListener, httpHandler)
+		}, func(error) {
+			httpListener.Close()
+		})
+	}
+	{
+		// This function just sits and waits for ctrl-C.
+		cancelInterrupt := make(chan struct{})
+		g.Add(func() error {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+			select {
+			case sig := <-c:
+				return fmt.Errorf("received signal %s", sig)
+			case <-cancelInterrupt:
+				return nil
+			}
+		}, func(error) {
+			close(cancelInterrupt)
+		})
+	}
+	logger.Log("exit", g.Run())
+}
+
+func usageFor(fs *flag.FlagSet, short string) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "USAGE\n")
+		fmt.Fprintf(os.Stderr, "  %s\n", short)
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "FLAGS\n")
+		w := tabwriter.NewWriter(os.Stderr, 0, 2, 2, ' ', 0)
+		fs.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(w, "\t-%s %s\t%s\n", f.Name, f.DefValue, f.Usage)
+		})
+		w.Flush()
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
