c1298fbb1b1f325ba22e941f57a1fff995db1062
antklim <antklim@gmail.com>
2018-04-14 18:25:15 +1000

    go-kit-greeter: comments added

--- go-kit-greeter/pkg/greetertransport/grpc.go
+++ go-kit-greeter/pkg/greetertransport/grpc.go
@@ -30,6 +30,7 @@ func NewGRPCServer(endpoints greeterendpoint.Endpoints, logger log.Logger) pb.Gr
 	}
 }
 
+// Greeting implementation of the method of the GreeterService interface.
 func (s *grpcServer) Greeting(ctx oldcontext.Context, req *pb.GreetingRequest) (*pb.GreetingResponse, error) {
 	_, res, err := s.greeter.ServeGRPC(ctx, req)
 	if err != nil {
@@ -38,11 +39,15 @@ func (s *grpcServer) Greeting(ctx oldcontext.Context, req *pb.GreetingRequest) (
 	return res.(*pb.GreetingResponse), nil
 }
 
+// decodeGRPCGreetingRequest is a transport/grpc.DecodeRequestFunc that converts
+// a gRPC greeting request to a user-domain greeting request.
 func decodeGRPCGreetingRequest(_ context.Context, grpcReq interface{}) (interface{}, error) {
 	req := grpcReq.(*pb.GreetingRequest)
 	return greeterendpoint.GreetingRequest{Name: req.Name}, nil
 }
 
+// encodeGRPCGreetingResponse is a transport/grpc.EncodeResponseFunc that converts
+// a user-domain greeting response to a gRPC greeting response.
 func encodeGRPCGreetingResponse(_ context.Context, response interface{}) (interface{}, error) {
 	res := response.(greeterendpoint.GreetingResponse)
 	return &pb.GreetingResponse{Greeting: res.Greeting}, nil
