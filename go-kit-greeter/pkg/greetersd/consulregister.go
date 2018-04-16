cc374b925b24b2dafb6360f895e8bbf6dc8005e7
antklim <antklim@gmail.com>
2018-04-16 17:34:05 +1000

    go-kit-greeter: Consul register added

---
+++ go-kit-greeter/pkg/greetersd/consulregister.go
@@ -0,0 +1,63 @@
+package greetersd
+
+import (
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/sd"
+	consulsd "github.com/go-kit/kit/sd/consul"
+	"github.com/hashicorp/consul/api"
+)
+
+// ConsulRegister method.
+func ConsulRegister(consulAddress string,
+	consulPort string,
+	advertiseAddress string,
+	advertisePort string) (registar sd.Registrar) {
+
+	// Logging domain.
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// Service discovery domain. In this example we use Consul.
+	var client consulsd.Client
+	{
+		consulConfig := api.DefaultConfig()
+		consulConfig.Address = consulAddress + ":" + consulPort
+		consulClient, err := api.NewClient(consulConfig)
+		if err != nil {
+			logger.Log("err", err)
+			os.Exit(1)
+		}
+		client = consulsd.NewClient(consulClient)
+	}
+
+	check := api.AgentServiceCheck{
+		HTTP:     "http://" + advertiseAddress + ":" + advertisePort + "/health",
+		Interval: "10s",
+		Timeout:  "1s",
+		Notes:    "Basic health checks",
+	}
+
+	port, _ := strconv.Atoi(advertisePort)
+	num := rand.Intn(100) // to make service ID unique
+	asr := api.AgentServiceRegistration{
+		ID:      "go.kit.srv.greeter" + strconv.Itoa(num), //unique service ID
+		Name:    "go.kit.srv.greeter",
+		Address: advertiseAddress,
+		Port:    port,
+		Tags:    []string{"greeter"},
+		Check:   &check,
+	}
+	registar = consulsd.NewRegistrar(client, &asr, logger)
+	return
+}
