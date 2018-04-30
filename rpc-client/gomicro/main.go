7013f3f673dc136e1144bdbde9b0198dcf7f3721
antklim <antklim@gmail.com>
2018-04-30 12:38:44 +1000

    go-micro-greeter: adding name cli argument to client

--- rpc-client/gomicro/main.go
+++ rpc-client/gomicro/main.go
@@ -5,16 +5,34 @@ import (
 	"fmt"
 
 	proto "github.com/antklim/go-microservices/go-micro-greeter/pb"
+	"github.com/micro/cli"
 	micro "github.com/micro/go-micro"
 )
 
 func main() {
-	service := micro.NewService(micro.Name("go-micro-srv-greeter.client"))
-	service.Init()
+	service := micro.NewService(
+		micro.Name("go-micro-srv-greeter.client"),
+		micro.Flags(
+			cli.StringFlag{
+				Name:  "name",
+				Usage: "Name to greet",
+			}),
+	)
+
+	var name string
+
+	service.Init(
+		micro.Action(func(c *cli.Context) {
+			name = "gomicro RPC call"
+			if len(c.String("name")) > 0 {
+				name = c.String("name")
+			}
+		}),
+	)
 
 	client := proto.NewGreeterClient("go-micro-srv-greeter", service.Client())
 
-	rsp, err := client.Greeting(context.Background(), &proto.GreetingRequest{Name: "gomicro RPC call"})
+	rsp, err := client.Greeting(context.Background(), &proto.GreetingRequest{Name: name})
 	if err != nil {
 		fmt.Println(err)
 	}
